ad97ff9b3d7dfe74f4b83f7b72ed5299361cbaba
hdrock03 <hardik.1rn16ec036@gmail.com>
2021-10-13 21:57:56 +0530

    added url

---
+++ app.js
@@ -0,0 +1,30 @@
+var button = document.querySelector("#button");
+var input = document.querySelector("#input");
+var output = document.querySelector("#output");
+
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong happend please try after sometime")
+}
+
+
+function clickHandler() {
+    var inputText = input.value; 
+
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            output.innerText = translatedText; // output
+           })
+        .catch(errorHandler)
+};
+
+button.addEventListener("click", clickHandler)
